24392caa1e9d46747c66ad299158c571f34dc16f
Vinicius Fraga <viniciusfrg@outlook.com.br>
2022-12-31 20:17:53 -0300

    applied layer application

---
+++ DevFreela.Application/InputModels/CreateUserInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.InputModels
+{
+    public class CreateUserInputModel
+    {
+        public CreateUserInputModel(string userName, string email, DateTime birthDate)
+        {
+            FullName = userName;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
--- DevFreela.Core/Entities/User.cs
+++ DevFreela.Core/Entities/User.cs
@@ -1,10 +1,11 @@
 ﻿namespace DevFreela.Core.Entities {
     public class User : BaseEntity
     {
-        public User(string fullName, string email, DateTime birthDate)
+        public User(string fullName, string email, string password, DateTime birthDate)
         {
             FullName = fullName;
             Email = email;
+            Password = password;
             BirthDate = birthDate;
             CreatedAt= DateTime.Now;
             Active = true;
@@ -16,6 +17,7 @@
 
         public string FullName { get; private set; }
         public string Email { get; private set; }
+        public string Password { get; private set; }
         public DateTime BirthDate { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public bool Active { get; set; }
@@ -23,5 +25,6 @@
         public List<UserSkill> Skills { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
         public List<Project> FreelanceProjects { get; set; }
+
     }
 }
