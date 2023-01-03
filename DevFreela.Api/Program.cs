f5d407aafba06a60ca1acc33599ff9d3a80eff28
Vinicius Fraga <viniciusfrg@outlook.com.br>
2023-01-03 19:18:56 -0300

    Create Connection String and application the migrations.

--- DevFreela.Api/Program.cs
+++ DevFreela.Api/Program.cs
@@ -2,6 +2,7 @@ using DevFreela.Api.Models;
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,8 +16,13 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
 });
-builder.Services.AddSingleton<DevFreelaDbContext>();
+
+var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
+builder.Services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ISkillService, SkillService>();
 
 builder.Services.AddScoped<ExampleClass>(e => new ExampleClass { 
 Name = "Initial Stage"
