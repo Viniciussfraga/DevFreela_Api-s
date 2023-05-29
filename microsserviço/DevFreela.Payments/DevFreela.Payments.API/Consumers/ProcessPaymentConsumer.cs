3a64d62a67c6634c4ccf29d8c4bfd9ad356bb8b0
Vinicius Fraga <110305977+Viniciussfraga@users.noreply.github.com>
2023-05-29 08:13:22 -0300

    microsserviço e consumidor

---
+++ microsserviço/DevFreela.Payments/DevFreela.Payments.API/Consumers/ProcessPaymentConsumer.cs
@@ -0,0 +1,89 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace DevFreela.Payments.API.Consumers
+{
+    //Classe que vai ficar rodando o serviço do consumidor
+    public class ProcessPaymentConsumer : BackgroundService
+    {
+        private const string QUEUE = "Payments";
+        private const string PAYMENT_APPROVED_QUEUE = "PaymentsApproved";
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly IServiceProvider _serviceProvider;
+
+        public ProcessPaymentConsumer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(
+                queue: QUEUE,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            _channel.QueueDeclare(
+                queue: PAYMENT_APPROVED_QUEUE,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //processar a mensagem
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (sender, EventArgs) =>
+            {
+                var byteArray = EventArgs.Body.ToArray(); //obtendo array de bytes
+                var paymentInfoJson = Encoding.UTF8.GetString(byteArray); //convertendo para stringJson
+
+                var paymentInfo = JsonSerializer.Deserialize<PaymentInfoInputModel>(paymentInfoJson); //Deserializando o json
+
+                ProcessPayment(paymentInfo);
+
+                var paymentApproved = new PaymentApprovedIntegrationEvent(paymentInfo.Id);
+                var paymentApprovedJson = JsonSerializer.Serialize(paymentApproved);
+                var paymentApprovedBytes = Encoding.UTF8.GetBytes(paymentApprovedJson);
+
+                _channel.BasicPublish(
+                    exchange:"",
+                    routingKey: PAYMENT_APPROVED_QUEUE,
+                    basicProperties: null,
+                    body: paymentApprovedBytes
+                    );
+
+                _channel.BasicAck(EventArgs.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(QUEUE, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public void ProcessPayment(PaymentInfoInputModel paymentInfo)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var paymentService = scope.ServiceProvider.GetRequiredService<IPaymentService>();
+
+                paymentService.Process(paymentInfo);
+            } 
+        }
+    }
+}
--- microsserviço/DevFreela.Payments/DevFreela.Payments.API/Program.cs
+++ microsserviço/DevFreela.Payments/DevFreela.Payments.API/Program.cs
@@ -1,3 +1,4 @@
+using DevFreela.Payments.API.Consumers;
 using DevFreela.Payments.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
+
+builder.Services.AddHostedService<ProcessPaymentConsumer>(); /*AddHostedService  útil quando você tem tarefas ou processos que precisam ser executados continuamente em segundo plano
+                                                             enquanto a aplicação estiver em execução, como processar pagamentos, enviar e-mails programados, atualizar caches,
+                                                             entre outras atividades. O uso de serviços hospedados facilita a implementação dessas tarefas e garante que elas sejam executadas
+                                                             corretamente no contexto do ciclo de vida da aplicação.*/
 
 var app = builder.Build();
 
